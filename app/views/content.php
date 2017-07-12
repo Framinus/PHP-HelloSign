b5cd07c348348be388f586f234ffc51c9e5e799f
Jen Young <jyoung488@gmail.com>
2017-07-12 14:27:04 -0700

    Refactor HTML indentation

--- app/views/content.php
+++ app/views/content.php
@@ -1,16 +1,27 @@
 <div>
-  <h3>What would you like to do?</h3>
-  <h4>Account Information</h4>
+  <h3>
+    What would you like to do?
+  </h3>
+  <h4>
+    Account Information
+  </h4>
   <ul>
-    <li><a href="account-info.php">Get Account</a></li>
+    <li>
+      <a href="account-info.php">Get Account</a>
+    </li>
   </ul>
 
-  <h4>Signature Requests</h4>
+  <h4>
+    Signature Requests
+  </h4>
   <ul>
-    <li>Get signature request: <form name="signature" action="get-sig-request.php" method="get">
-      <input type="text" name="request" placeholder="signature request id">
-      <input type="submit">
-    </form></li>
+    <li>
+      Get signature request:
+      <form name="signature" action="get-sig-request.php" method="get">
+        <input type="text" name="request" placeholder="signature request id">
+        <input type="submit">
+      </form>
+    </li>
     <li>
       <a href="list-sig-requests.php">List signature requests</a>
     </li>
@@ -20,38 +31,63 @@
     <li>
       <a href="send-with-template.php">Send Signature Request with Template</a>
     </li>
-    <li>Download signature request: <form name="signature" action="download.php" method="get">
-      <input type="text" name="request" placeholder="signature request id">
-      <input type="submit">
-    </form></li>
+    <li>
+      Download signature request:
+      <form name="signature" action="download.php" method="get">
+        <input type="text" name="request" placeholder="signature request id">
+        <input type="submit">
+      </form>
+    </li>
   </ul>
-  <h4>Templates</h4>
+  <h4>
+    Templates
+  </h4>
   <ul>
-    <li>Get template: <form name="template" action="get-template.php" method="get">
-      <input type="text" name="template" placeholder="template id">
-      <input type="submit">
-    </form></li>
-    <li><a href="list-templates.php">List Templates</a></li>
+    <li>Get template:
+      <form name="template" action="get-template.php" method="get">
+        <input type="text" name="template" placeholder="template id">
+        <input type="submit">
+      </form>
+    </li>
+    <li>
+      <a href="list-templates.php">List Templates</a>
+    </li>
   </ul>
   <h4>Embedded Signing</h4>
   <ul>
-    <li><a href="embedded-sig-request.php">Send Embedded Signature Request</a></li>
-    <li><a href="embedded-template-request.php">Send Embedded Signature Request with Template</a></li>
+    <li>
+      <a href="embedded-sig-request.php">Send Embedded Signature Request</a>
+    </li>
+    <li>
+      <a href="embedded-template-request.php">Send Embedded Signature Request with Template</a>
+    </li>
   </ul>
   <h4>Embedded Templates</h4>
   <ul>
-    <li><a href="embedded-template.php">Create embedded template</a></li>
-    <li>Edit template: <form name="template" action="edit-embedded-template.php" method="get">
-      <input type="text" name="template" placeholder="template id">
-      <input type="submit">
-    </form></li>
-    <li>Preview signature request with template: <form name="template" action="preview-template-request.php" method="get">
-      <input type="text" name="template" placeholder="template id">
-      <input type="submit">
-    </form></li>
+    <li>
+      <a href="embedded-template.php">Create embedded template</a>
+    </li>
+    <li>
+      Edit template:
+      <form name="template" action="edit-embedded-template.php" method="get">
+        <input type="text" name="template" placeholder="template id">
+        <input type="submit">
+      </form>
+    </li>
+    <li>
+      Preview signature request with template:
+      <form name="template" action="preview-template-request.php" method="get">
+        <input type="text" name="template" placeholder="template id">
+        <input type="submit">
+      </form>
+    </li>
   </ul>
-  <h4>Embedded Requesting</h4>
+  <h4>
+    Embedded Requesting
+  </h4>
   <ul>
     <li><a href="embedded-request.php">Embedded Request</a></li>
   </ul>
+  <h4>
+  </h4>
 </div>
